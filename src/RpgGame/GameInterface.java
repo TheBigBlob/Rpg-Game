02c041a73cdb5f3a3697c7d8bd731458f32cb1c1
TheBigBlob <88168493+TheBigBlob@users.noreply.github.com>
2024-01-22 18:23:44 -0800

    0.1

--- src/RpgGame/GameInterface.java
+++ src/RpgGame/GameInterface.java
@@ -18,7 +18,7 @@ public class GameInterface {
            GameInterface.runGame();
         }
     }
-
+    //bozo
     public static void runGame() {
         createEnemy.add(new Enemy("Slime", 10, 1, 0));
         createEnemy.add(new Enemy("Goblin", 5, 2, 1));
